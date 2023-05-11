1f75782df980bf3b48aacb2acd577fdb7385e469
Volodymyr Chubenko <volodymyr.chubenko@sisense.com>
2023-05-11 10:00:58 +0300

    Update 18 les

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -24,12 +24,11 @@ const controlRecipes = async function () {
     // 2) Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (error) {
-    alert(error);
+    recipeView.renderError();
   }
 };
 
-// controlRecipes();
-
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes)
-);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
